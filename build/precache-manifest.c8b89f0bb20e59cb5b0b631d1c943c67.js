c05fa60fd30041d880808a27af8a3852c293c097
ELITA04 <elita041999@gmail.com>
2020-03-09 20:42:17 +0530

    Initial commit

---
+++ build/precache-manifest.c8b89f0bb20e59cb5b0b631d1c943c67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f8ca1dd087d7986b989a40e60c290b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ad0fa82e6d9ded1c16dc",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b56d1bcb0dc07612fb10",
+    "url": "/portfolio/static/js/2.0ebeb635.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.0ebeb635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0fa82e6d9ded1c16dc",
+    "url": "/portfolio/static/js/main.a126ba5f.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.54de3b62c1ba38871e34f3bc33e2f62a.js"
+  "/portfolio/precache-manifest.c8b89f0bb20e59cb5b0b631d1c943c67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
