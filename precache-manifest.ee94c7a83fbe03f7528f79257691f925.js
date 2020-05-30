2605e37a30bd8e701b9ffe3333c290eede01a593
elita <elita041999@gmail.com>
2020-05-30 13:17:36 +0530

    Updates

---
+++ precache-manifest.ee94c7a83fbe03f7528f79257691f925.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3188085b8efea19c1e3a41a0a13d4ad",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "e2f538370fb81845b148",
+    "url": "/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3492b440d0344b6b0f2",
+    "url": "/website/static/js/2.c43699c4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/website/static/js/2.c43699c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f538370fb81845b148",
+    "url": "/website/static/js/main.ab1c3062.chunk.js"
+  },
+  {
+    "revision": "13a3a53d375ffd936166",
+    "url": "/website/static/js/runtime-main.4de80ac7.js"
+  },
+  {
+    "revision": "5a8577ce298c4fe39893f59212dbc580",
+    "url": "/website/static/media/ElitaMenezes.5a8577ce.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bb7c5d3a0b10b405b41158db08cd5133.js"
+  "/website/precache-manifest.ee94c7a83fbe03f7528f79257691f925.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
